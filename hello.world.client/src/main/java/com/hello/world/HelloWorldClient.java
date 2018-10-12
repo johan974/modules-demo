c38c3e4b963fc6eff0b320fa3be97c2e577edc3b
johan974 <jj974@upcmail.nl>
2018-10-12 20:05:48 +0200

    Module base version

---
+++ hello.world.client/src/main/java/com/hello/world/HelloWorldClient.java
@@ -0,0 +1,10 @@
+package com.hello.world;
+
+import com.hello.HelloWorld;
+
+public class HelloWorldClient {
+    public static void main (String arg[]) {
+        HelloWorld hello = new HelloWorld();
+        System.out.println(hello.sayHelloWorld());
+    }
+}
---
+++ module1/src/main/java/com/hello/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.hello;
+
+public class HelloWorld {
+    public String sayHelloWorld() {
+        return "Hello World!";
+    }
+}
